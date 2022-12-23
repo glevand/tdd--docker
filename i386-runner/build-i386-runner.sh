5a86b1ea37382ad7752011833fd37ccd7980fdfb
Geoff Levand <geoff@infradead.org>
2022-12-22 17:38:48 -0800

    i386-runner: Updates
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- i386-runner/build-i386-runner.sh
+++ i386-runner/build-i386-runner.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ${JENKINS_URL:-} ]]; then
-	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
-else
-	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
-fi
-
-set -e
-
-script_name="${0##*/}"
-
-real_source="$(realpath "${BASH_SOURCE}")"
-DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
-
-project_name='i386-runner'
-project_from='i386_debian'
-project_description='Builds a i386 Debian 11 Bullseye based Docker image with libstdc++5 support.'
-
-VERSION=${VERSION:-"2"}
-
-extra_build_args='--platform=linux/386'
-
-build_on_exit() {
-	true
-}
-
 docker_build_setup() {
 	true
 }
@@ -33,4 +8,28 @@ host_install_extra() {
 	true
 }
 
-source "${DOCKER_TOP}/build-common.sh"
+build_on_exit() {
+	true
+}
+
+#===============================================================================
+if [[ ${JENKINS_URL} ]]; then
+	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
+else
+	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
+fi
+
+script_name="${0##*/}"
+
+real_source="$(realpath "${BASH_SOURCE[0]}")"
+DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
+
+export project_name='i386-runner'
+export project_from='i386_debian'
+export project_description='Builds a i386 Debian 11 Bullseye based Docker image with libstdc++5 support.'
+
+VERSION=${VERSION:-"3"}
+
+export extra_build_args='--platform=linux/386'
+
+source ${DOCKER_TOP}/build-common.sh
