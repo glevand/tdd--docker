1c81ad271b08ff654d35cc53481fd03a3d677e1b
Geoff Levand <geoff@infradead.org>
2022-12-23 18:19:42 -0800

    yocto-builder: Updates
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- yocto-builder-jessie/build-yocto-builder.sh
+++ yocto-builder-jessie/build-yocto-builder.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ${JENKINS_URL:-} ]]; then
-	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
-else
-	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
-fi
-
-set -e
-
-script_name="${0##*/}"
-
-real_source="$(realpath "${BASH_SOURCE}")"
-DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
-
-project_name='yocto-builder-jessie'
-project_from='debian_jessie'
-project_description='Image that contains tools for working with old, end-of-life Yocto.'
-
-VERSION=${VERSION:-"4"}
-
 build_on_exit() {
 	true
 }
@@ -30,5 +11,25 @@ docker_build_setup() {
 host_install_extra() {
 	true
 }
+
+#===============================================================================
+if [[ ${JENKINS_URL} ]]; then
+	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
+else
+	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
+fi
+
+script_name="${0##*/}"
+
+real_source="$(realpath "${BASH_SOURCE[0]}")"
+DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
+
+export project_name='yocto-builder-jessie'
+export project_from='debian_jessie'
+export project_description='Builds a Debian 8 Jessie based image that contains tools for working with old, end-of-life Yocto.'
+
+VERSION="${VERSION:-5}"
+
+export extra_build_args=''
 
 source ${DOCKER_TOP}/build-common.sh
--- yocto-builder/build-yocto-builder.sh
+++ yocto-builder/build-yocto-builder.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ${JENKINS_URL:-} ]]; then
-	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
-else
-	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
-fi
-
-set -e
-
-script_name="${0##*/}"
-
-real_source="$(realpath "${BASH_SOURCE}")"
-DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
-
-project_name='yocto-builder'
-project_from='debian'
-project_description='Builds a docker image that contains tools for working with Yocto.'
-
-VERSION=${VERSION:-"5"}
-
-build_on_exit() {
-	true
-}
-
 docker_build_setup() {
 	true
 }
@@ -30,5 +7,29 @@ docker_build_setup() {
 host_install_extra() {
 	true
 }
+
+build_on_exit() {
+	true
+}
+
+#===============================================================================
+if [[ ${JENKINS_URL} ]]; then
+	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
+else
+	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
+fi
+
+script_name="${0##*/}"
+
+real_source="$(realpath "${BASH_SOURCE[0]}")"
+DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
+
+export project_name='yocto-builder'
+export project_from='debian'
+export project_description='Builds a Debian 11 Bullseye based image that contains tools for working with the Yocto project.'
+
+VERSION="${VERSION:-6}"
+
+export extra_build_args=''
 
 source ${DOCKER_TOP}/build-common.sh
