2305bcc5ad1eb31235a97885bf3ee44933ccca5b
Geoff Levand <geoff@infradead.org>
2022-12-25 18:20:47 -0800

    sbu-builder: Updates
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- sbu-builder/build-sbu-builder.sh
+++ sbu-builder/build-sbu-builder.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ${JENKINS_URL:-} ]]; then
-	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
-else
-	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
-fi
-
-set -e
-
-script_name="${0##*/}"
-
-real_source="$(realpath "${BASH_SOURCE}")"
-DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
-
-project_name='sbu-builder'
-project_from='debian'
-project_description='Builds a docker image that contains tools for working with the Secure Boot Utils Project.'
-
-VERSION=${VERSION:-"1"}
-
-build_on_exit() {
-	true
-}
-
 docker_build_setup() {
 	true
 }
@@ -30,5 +7,29 @@ docker_build_setup() {
 host_install_extra() {
 	true
 }
+
+build_on_exit() {
+	true
+}
+
+#===============================================================================
+if [[ ${JENKINS_URL} ]]; then
+	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
+else
+	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
+fi
+
+script_name="${0##*/}"
+
+real_source="$(realpath "${BASH_SOURCE[0]}")"
+DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
+
+export project_name='sbu-builder'
+export project_from='debian'
+export project_description='Builds a docker image that contains tools for working with the Secure Boot Utils Project.'
+
+VERSION="${VERSION:-2}"
+
+export extra_build_args=''
 
 source ${DOCKER_TOP}/build-common.sh
