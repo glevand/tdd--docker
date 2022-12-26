03132c59d44629b0e790d029a5fec77a1eeb0fdb
Geoff Levand <geoff@infradead.org>
2022-12-26 15:02:04 -0800

    rmt: Updates
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- rmt/build-rmt.sh
+++ rmt/build-rmt.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ${JENKINS_URL:-} ]]; then
-	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
-else
-	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
-fi
-
-set -e
-
-script_name="${0##*/}"
-
-real_source="$(realpath "${BASH_SOURCE}")"
-DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
-
-project_name='tdd-rmt'
-project_from='opensuse'
-project_description='Builds a docker image that contains the SUSE RMT Repository Mirroring Tool server.'
-
-VERSION=${VERSION:-"1"}
-DOCKER_NAME=${DOCKER_NAME:-"rmt-server"}
-
-curr_dir="$(pwd)"
-
 build_on_exit() {
 	true
 }
@@ -33,5 +11,29 @@ docker_build_setup() {
 host_install_extra() {
 	sudo cp -vf ${curr_dir}/rmt.conf /etc/rmt.conf
 }
+
+#===============================================================================
+if [[ ${JENKINS_URL} ]]; then
+	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
+else
+	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
+fi
+
+script_name="${0##*/}"
+
+real_source="$(realpath "${BASH_SOURCE[0]}")"
+DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
+
+export project_name='tdd-rmt'
+export project_from='opensuse'
+export project_description='Builds a docker image that contains the SUSE RMT Repository Mirroring Tool server.'
+
+VERSION="${VERSION:-2}"
+
+DOCKER_NAME=${DOCKER_NAME:-"rmt-server"}
+
+curr_dir="$(pwd)"
+
+export extra_build_args=''
 
 source ${DOCKER_TOP}/build-common.sh
