3146a8be05f78f491b5c9e16433f4a88f5a96516
Geoff Levand <geoff@infradead.org>
2022-12-27 17:49:21 -0800

    tftpd: Updates
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- tftpd/build-tftpd.sh
+++ tftpd/build-tftpd.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ${JENKINS_URL:-} ]]; then
-	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
-else
-	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
-fi
-
-set -e
-
-script_name="${0##*/}"
-
-real_source="$(realpath "${BASH_SOURCE}")"
-DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
-
-project_name='tdd-tftpd'
-project_from='alpine'
-project_description='Builds a minimal docker image that contains tftpd-hpa.'
-
-VERSION=${VERSION:-"1"}
-
 build_on_exit() {
 	true
 }
@@ -30,5 +11,25 @@ docker_build_setup() {
 host_install_extra() {
 	true
 }
+
+#===============================================================================
+if [[ ${JENKINS_URL} ]]; then
+	export PS4='+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):'
+else
+	export PS4='\[\e[0;33m\]+ ${BASH_SOURCE##*/}:${LINENO}:(${FUNCNAME[0]:-main}):\[\e[0m\] '
+fi
+
+script_name="${0##*/}"
+
+real_source="$(realpath "${BASH_SOURCE[0]}")"
+DOCKER_TOP="$(realpath "${DOCKER_TOP:-${real_source%/*}/..}")"
+
+export project_name='tdd-tftpd'
+export project_from='alpine'
+export project_description='Builds a minimal Alpine Linux docker image that contains tftpd-hpa.'
+
+VERSION="${VERSION:-2}"
+
+export extra_build_args=''
 
 source ${DOCKER_TOP}/build-common.sh
